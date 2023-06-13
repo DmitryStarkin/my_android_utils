4bb82faf8d51e0ac26d0255d64f3aa7e96e40bf2
Dmitry <t0506803080@gmail.com>
2023-06-13 12:07:43 +0300

    fun added

--- myandroidutil/src/main/java/com/starsoft/myandroidutil/collectionUtils/CollectionUtils.kt
+++ myandroidutil/src/main/java/com/starsoft/myandroidutil/collectionUtils/CollectionUtils.kt
@@ -19,31 +19,6 @@ package com.starsoft.myandroidutil.collectionUtils
  * Created by Dmitry Starkin on 24.05.2022 20:34.
  */
 
-fun <T> Iterable<T>.removeAdjacent(): List<T> {
-    var last: T? = null
-    return mapNotNull {
-        if (it == last) {
-            null
-        } else {
-            last = it
-            it
-        }
-    }
-}
-
-
-fun <T> listOfCollections(vararg elements: Collection<T>): List<T> = if (elements.isNotEmpty()) {
-    ArrayList<T>().apply {
-        elements.forEach {
-            addAll(it)
-        }
-    }
-} else {emptyList()}
-
-
-fun <T>  Collection<T>.isLastIndex(index: Int): Boolean = (size - 1) == index
-
-
 fun <T> Array<T>.getNext(item: T): T{
 
     forEachIndexed { index, currentItem ->
@@ -58,6 +33,15 @@ fun <T> Array<T>.getNext(item: T): T{
     return this[0]
 }
 
+fun <T> listOfCollections(vararg elements: Collection<T>): List<T> = if (elements.isNotEmpty()) {
+    ArrayList<T>().apply {
+        elements.forEach {
+            addAll(it)
+        }
+    }.toList()
+} else {emptyList()}
+
+
 fun <T> List<T>.removeLast(): List<T> =
     if(isEmpty()){
         this
@@ -66,8 +50,57 @@ fun <T> List<T>.removeLast(): List<T> =
         this
     }
 
-fun <T> List<T>.add(item: T) =
+fun <T> List<T>.removeFirst(): List<T> =
+    if(isEmpty()){
+        this
+    } else {
+        (this as MutableList<T>).removeAt(0)
+        this
+    }
+
+fun <T> List<T>.remove(item: T): List<T> =
+    if(isEmpty()){
+        this
+    } else {
+        val newList = ArrayList<T>().also {
+            it.addAll(this)
+        }
+        newList.remove(item)
+        newList
+    }
+
+fun <T> List<T>.add(item: T) : List<T>  = if(this.isNotEmpty()){
     (this as MutableList<T>).add(item)
+    this
+} else {
+    ArrayList<T>().also {
+        it.add(item)
+    }.toList()
+}
+
+fun <T> List<T>.addToFront(item: T): List<T>  = if(this.isNotEmpty()){
+    (this as MutableList<T>).add(0, item)
+    this
+} else {
+    ArrayList<T>().also {
+        it.add(item)
+    }.toList()
+}
+
+fun <T> Iterable<T>.removeAdjacent(): List<T> {
+    var last: T? = null
+    return mapNotNull {
+        if (it == last) {
+            null
+        } else {
+            last = it
+            it
+        }
+    }
+}
+
+fun <T>  Collection<T>.isLastIndex(index: Int): Boolean = (size - 1) == index
+
 
 fun <T> List<T>.addAndReturnNewInstance(item: T): List<T> = ArrayList<T>().also {
     it.addAll(this)
@@ -90,3 +123,34 @@ fun <T> Collection<T>.containsAnyItemFrom(other: Collection<T>): Boolean{
     }
     return false
 }
+
+fun <T, R : Comparable<R>> Iterable<T>.groupByDescending(selector: (T) -> R): List<List<T>> =
+    sortedByDescending {
+        selector(it)
+    }.group(selector)
+
+fun <T, R : Comparable<R>> Iterable<T>.groupBy(selector: (T) -> R): List<List<T>> =
+    sortedBy {
+        selector(it)
+    }.group(selector)
+
+private fun <T, R : Comparable<R>> Iterable<T>.group(selector: (T) -> R): List<List<T>> =
+    let {
+        val result = ArrayList<List<T>>()
+        val value = ArrayList<T>()
+        it.forEach { item ->
+            if (value.isEmpty()) {
+                value.add(item)
+            } else if (selector(value[0]) == selector(item)) {
+                value.add(item)
+            } else {
+                result.add(value.toList())
+                value.clear()
+                value.add(item)
+            }
+        }
+        if(value.isNotEmpty()){
+            result.add(value.toList())
+        }
+        result.toList()
+    }
